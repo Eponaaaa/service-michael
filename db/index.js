9fe376c6f6118f27e08becf694fbaae9a19bc946
Michael Johnson <hack.reactor.mj0730@gmail.com>
2020-03-13 18:52:47 -0500

    generate 100 fake records for database

--- db/index.js
+++ db/index.js
@@ -1,11 +1,27 @@
 const {Sequelize} = require ('sequelize');
 
-module.exports = new Sequelize('gallery', 'root', 'root', {
+const db = new Sequelize('gallery', 'root', 'root', {
   host: 'localhost',
   dialect: 'mysql'
 });
 
+const Photos = db.define('photos', {
+  url0: Sequelize.text,
+  url1: Sequelize.text,
+  url2: Sequelize.text,
+  url3: Sequelize.text,
+  url4: Sequelize.text,
+  url5: Sequelize.text,
+  url6: Sequelize.text,
+  url7: Sequelize.text,
+  url8: Sequelize.text,
+  url9: Sequelize.text,
+});
+
 //to test the connection
-// db.authenticate()
-//   .then(() => console.log('Database Connected'))
-//   .catch(() => console.log('Error connecting to database'));
+db.authenticate()
+  .then(() => console.log('Database Connected'))
+  .catch(() => console.log('Error connecting to database'));
+
+module.exports.db = db;
+module.exports.photos = Photos;
