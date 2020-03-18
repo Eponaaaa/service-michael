3893c4fc43a4701647030cd76ffe657cef05a1a3
Michael Johnson <mrj0730@icloud.com>
2020-03-18 16:31:04 -0500

    added /photos route, api returns correct data

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyparser = require('body-parser');
+const db = require('../db/index.js');
+const Sequelize = require('sequelize');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -10,6 +12,15 @@ app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
   res.sendStatus(200);
+});
+
+//currently gets and returns all rows of the database. It's only returning file names, not the full path.
+app.get('/photos', (req, res) => {
+  db.Photos.findAll()
+    .then(item => {
+      res.json(item);
+    })
+    .catch(err => console.log(err));
 });
 
 app.listen(PORT, console.log(`Server listening on ${PORT}`));
