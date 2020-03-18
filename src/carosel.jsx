9f765e6634021cbcace1af1c82b51b97dac227aa
Michael Johnson <mrj0730@icloud.com>
2020-03-18 16:47:23 -0500

    added jsx modules and some design notes

---
+++ src/carosel.jsx
@@ -0,0 +1,10 @@
+/*
+heart icon in upper right corner to favorite- clicking fills in color
+below heart icon is expand image icon. containers for both are 36x36
+clicking on the image brings it out into a light box
+next/prev buttons are on far left & right side of image, vertically centered
+  circular white buttons with a drop shadow, arrows are black single chevrons (svg can be downloaded from etsy)
+  chevron box is 24x24px, button is 48x48
+  drop shadow slightly darkens on hover
+main image ~ 625px wide
+*/
---
+++ src/thumbs.jsx
@@ -0,0 +1,6 @@
+/*
+thumbs are 56x56px w/ border radius ~5px
+hovering over thumb puts a 2px black border and changes the image to the active in carosel
+unselected thumbs have a white opaque overlay to fade it slightly
+thumbs are aligned vertically along left side of screen, non-responsive
+*/
