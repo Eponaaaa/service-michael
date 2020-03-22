952d8cdf767428714bf2f7d87c2aaf6c304f5be4
Michael Johnson <mrj0730@icloud.com>
2020-03-22 02:59:22 -0500

    cleaning out comments

--- src/thumbs.jsx
+++ src/thumbs.jsx
@@ -1,10 +1,3 @@
-/*
-thumbs are 56x56px w/ border radius ~5px
-hovering over thumb puts a 2px black border and changes the image to the active in carosel
-unselected thumbs have a white opaque overlay to fade it slightly
-thumbs are aligned vertically along left side of screen, non-responsive
-*/
-
 import React from 'react';
 import './styles/styles.css';
 
