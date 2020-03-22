df355d19b87d3626ccd9a05f7b890e788e6015a7
Michael Johnson <mrj0730@icloud.com>
2020-03-22 02:52:27 -0500

    refactor fav heart click handler

--- src/Carousel.jsx
+++ src/Carousel.jsx
@@ -17,19 +17,17 @@ class Carousel extends React.Component {
     super(props);
 
     this.state = {
-      favorite: false
+      favorite: false,
+      hideTheHeart: 'none'
     };
   }
 
   favoriteItem() {
-    if (this.state.favorite) {
-      document.getElementById('fav-heart-icon').setAttribute('style', 'display: none;');
-      this.setState({favorite: false});
-    } else {
-      document.getElementById('fav-heart-icon').setAttribute('style', 'display: block;');
-      this.setState({favorite: true});
-    }
+    this.state.favorite ?
+      this.setState({favorite: false, hideTheHeart: 'none'}) :
+      this.setState({favorite: true, hideTheHeart: 'block'});
   }
+
 
   nextImg() {
     let i = this.props.images.indexOf(this.props.currentImage);
@@ -81,7 +79,7 @@ class Carousel extends React.Component {
         <div className="button-container">
           <button id="heart-icon" onClick={this.favoriteItem.bind(this)}><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true" focusable="false"><path d="M12,21C10.349,21,2,14.688,2,9,2,5.579,4.364,3,7.5,3A6.912,6.912,0,0,1,12,5.051,6.953,6.953,0,0,1,16.5,3C19.636,3,22,5.579,22,9,22,14.688,13.651,21,12,21ZM7.5,5C5.472,5,4,6.683,4,9c0,4.108,6.432,9.325,8,10,1.564-.657,8-5.832,8-10,0-2.317-1.472-4-3.5-4-1.979,0-3.7,2.105-3.721,2.127L11.991,8.1,11.216,7.12C11.186,7.083,9.5,5,7.5,5Z"></path></svg></button>
 
-          <button id="fav-heart-icon" onClick={this.favoriteItem.bind(this)}><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true" focusable="false"><path d="M16.5,3A6.953,6.953,0,0,0,12,5.051,6.912,6.912,0,0,0,7.5,3C4.364,3,2,5.579,2,9c0,5.688,8.349,12,10,12S22,14.688,22,9C22,5.579,19.636,3,16.5,3Z"></path></svg></button>
+          <button id="fav-heart-icon" style={{display: this.state.hideTheHeart}} onClick={this.favoriteItem.bind(this)}><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true" focusable="false"><path d="M16.5,3A6.953,6.953,0,0,0,12,5.051,6.912,6.912,0,0,0,7.5,3C4.364,3,2,5.579,2,9c0,5.688,8.349,12,10,12S22,14.688,22,9C22,5.579,19.636,3,16.5,3Z"></path></svg></button>
 
           <button id="expand" onClick={this.openLightbox.bind(this)}>
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true" focusable="false"><path d="M15,3c-0.553,0-1,0.447-1,1s0.447,1,1,1h2.586l-4.293,4.293c-0.391,0.391-0.391,1.023,0,1.414 C13.488,10.902,13.744,11,14,11s0.512-0.098,0.707-0.293L19,6.414V9c0,0.553,0.447,1,1,1s1-0.447,1-1V3H15z"></path> <path d="M9.293,13.293L5,17.586V15c0-0.553-0.447-1-1-1s-1,0.447-1,1v6h6c0.553,0,1-0.447,1-1s-0.447-1-1-1H6.414l4.293-4.293 c0.391-0.391,0.391-1.023,0-1.414S9.684,12.902,9.293,13.293z"></path></svg>
