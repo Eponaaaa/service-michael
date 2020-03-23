30d90f1bd62e99df3625ac1079a2c18c01428579
Michael Johnson <mrj0730@icloud.com>
2020-03-23 12:26:13 -0500

    spelling corrections

--- src/Carousel.jsx
+++ src/Carousel.jsx
@@ -44,11 +44,11 @@ class Carousel extends React.Component {
   }
 
   openLightbox() {
-    document.getElementById('carosel-lightbox').setAttribute('style', 'display: block;');
+    document.getElementById('carousel-lightbox').setAttribute('style', 'display: block;');
   }
 
   closeLightbox() {
-    document.getElementById('carosel-lightbox').setAttribute('style', 'display: none;');
+    document.getElementById('carousel-lightbox').setAttribute('style', 'display: none;');
   }
 
   render() {
@@ -74,7 +74,7 @@ class Carousel extends React.Component {
           </button>
         </div>
 
-        <div id="carosel-lightbox">
+        <div id="carousel-lightbox">
           <img src={this.props.BASE_URL + this.props.currentImage} alt="" />
           <button id="close-lightbox" onClick={this.closeLightbox.bind(this)}><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true" focusable="false"><path d="M13.414,12l6.293-6.293a1,1,0,0,0-1.414-1.414L12,10.586,5.707,4.293A1,1,0,0,0,4.293,5.707L10.586,12,4.293,18.293a1,1,0,1,0,1.414,1.414L12,13.414l6.293,6.293a1,1,0,0,0,1.414-1.414Z"></path></svg></button>
         </div>
