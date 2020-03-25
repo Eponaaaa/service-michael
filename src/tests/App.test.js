bfedc667b8f81f9ffc6a80f82a671055a7de815d
Michael Johnson <mrj0730@icloud.com>
2020-03-24 19:26:40 -0500

    minor css change

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom';
 import { shallow } from 'enzyme';
 import App from '../App';
 
-describe('Some Cool Name', () => {
+const wrapper = shallow(<App />);
+
+describe('App Module Tests', () => {
 
   test('App renders without crashing', () => {
     const div = document.createElement('div');
@@ -11,6 +13,21 @@ describe('Some Cool Name', () => {
     ReactDOM.unmountComponentAtNode(div);
   });
 
+  test('State property imageSet is an array', () => {
+    expect(Array.isArray(wrapper.state().imageSet)).toEqual(true);
+  });
+
+  test('State property imageSet is a string', () => {
+    expect(typeof wrapper.state().currentImage).toBe('string');
+  });
+
+  test('Current image property is not storing a full URL', () => {
+    expect(wrapper.state().currentImage.startsWith('http')).toEqual(false);
+  });
+
+  test('setCurrentImage changes the currentImage state property', () => {
+ 
+  });
 } );
 
 
